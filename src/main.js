289c5091e3d168e3a74b8628547a47003cce4a92
Claudenir Nojosa <clau.nojosaf@gmail.com>
2023-05-13 10:45:58 -0300

    Adicionando header

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 document.addEventListener("DOMContentLoaded", function (){
     const buttons = document.querySelectorAll("[data-tab-button]");
     const questions = document.querySelectorAll("[data-faq-question]");
+    const heroSection = document.querySelector(".hero");
+    const alturaHero = heroSection.clientHeight;
 
+    window.addEventListener("scroll", function() {
+        const actualPosition = window.scrollY
+
+        if (actualPosition < alturaHero) {
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    })
+
+       //Seção de atrações 
     for (let i = 0; i<buttons.length; i++) {
         buttons[i].addEventListener("click", function (botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -12,11 +25,20 @@ document.addEventListener("DOMContentLoaded", function (){
             botao.target.classList.add("shows__tabs__button--is-active");
         })
     }
-
+       //Seção FAQ 
     for (let i = 0; i <questions.length; i++) {
         questions[i].addEventListener("click", abreOuFechaResposta);
     }
 })
+
+function ocultaElementosDoHeader() {
+    const header = document.querySelector("header");
+    header.classList.add("header--is-hidden");
+}
+function exibeElementosDoHeader() {
+    const header = document.querySelector("header");
+    header.classList.remove("header--is-hidden");
+}
 
 function abreOuFechaResposta(elemento) {
     const classe = "faq__questions__item--is-open";
@@ -24,7 +46,6 @@ function abreOuFechaResposta(elemento) {
 
     elementoPai.classList.toggle(classe);
 }
-
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll("[data-tab-button]");
